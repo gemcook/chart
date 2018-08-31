0534a8c91b6a4dcde9661bf65676d8475b228d8b
shsssskn <shsssskn@gmail.com>
2018-08-31 23:01:13 +0900

    [fix] ### rollup

--- scripts/rollup/umd.build.js
+++ scripts/rollup/umd.build.js
@@ -1,10 +1,14 @@
 const {rollup} = require('rollup');
-const fs = require('fs');
 const babel = require('rollup-plugin-babel');
 const closure = require('rollup-plugin-closure-compiler-js');
+const commonjs = require('rollup-plugin-commonjs');
+const prettier = require('rollup-plugin-prettier');
 const replace = require('rollup-plugin-replace');
+// const stripBanner = require('rollup-plugin-strip-banner');
+const json = require('rollup-plugin-json');
+const url = require('rollup-plugin-url');
 const resolve = require('rollup-plugin-node-resolve');
-const inject = require('rollup-plugin-inject');
+const postcss = require('rollup-plugin-postcss');
 const {
   getBabelOptions,
   resolvePath,
@@ -15,38 +19,50 @@ const {
 const isProduction = process.env.NODE_ENV === 'production';
 
 async function build() {
-  const bundle = await rollup({
-    input: resolvePath('src/index.js'),
-    external: isExternal,
-    plugins: [
-      resolve({
-        jsnext: true,
-        customResolveOptions: {
-          moduleDirectory: resolvePath('node_modules'),
-        },
-      }),
-      inject({
-        modules: {
-          $: 'jquery',
-          R: 'ramda',
-        },
-      }),
-      babel(getBabelOptions()),
-      replace({
-        'process.env.NODE_ENV': isProduction ? "'production'" : "'development'",
-      }),
-      closure(getClosureOptions()),
-    ],
-  });
+  try {
+    const bundle = await rollup({
+      input: resolvePath('src/index.js'),
+      external: isExternal,
+      plugins: [
+        resolve({
+          extensions: ['.js', '.json', '.jsx'],
+          preferBuiltins: false,
+          customResolveOptions: {
+            moduleDirectory: resolvePath('node_modules'),
+          },
+        }),
+        json(),
+        url(),
+        postcss({
+          extensions: ['.css'],
+        }),
+        commonjs({
+          include: 'node_modules/**',
+        }),
+        babel(getBabelOptions()),
+        replace({
+          'process.env.NODE_ENV': isProduction
+            ? "'production'"
+            : "'development'",
+        }),
+        closure(getClosureOptions()),
+        // TODO: COPYRIGHT
+        // stripBanner(),
+        isProduction &&
+          prettier({
+            parser: 'babylon',
+          }),
+      ],
+    });
 
-  const {code, map} = bundle.generate({
-    format: 'umd',
-    name: '@gemcook/chart',
-    sourceMap: true,
-  });
-
-  fs.writeFileSync(resolvePath('lib/index.js'), code);
-  fs.writeFileSync(resolvePath('lib/index.js.map'), map);
+    bundle.write({
+      format: 'umd',
+      file: resolvePath('lib/index.js'),
+      name: '@gemcook/chart',
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 build();
--- scripts/rollup/utils/getBabelOptions.js
+++ scripts/rollup/utils/getBabelOptions.js
@@ -4,26 +4,18 @@ module.exports = function getBabelOptions() {
     {
       exclude: 'node_modules/**',
       babelrc: false,
+      runtimeHelpers: true,
       presets: [
         '@babel/flow',
         '@babel/react',
         [
           '@babel/preset-env',
           {
-            targets: {browsers: ['> 1% in JP']},
-            modules: false,
+            targets: {browsers: ['> 1% in JP'], esmodules: false},
           },
         ],
       ],
-      plugins: [
-        ['@babel/plugin-proposal-decorators', {legacy: true}],
-        '@babel/plugin-proposal-export-namespace-from',
-        '@babel/plugin-proposal-throw-expressions',
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-syntax-import-meta',
-        ['@babel/plugin-proposal-class-properties', {loose: true}],
-        '@babel/plugin-proposal-json-strings',
-      ],
+      plugins: [['@babel/plugin-proposal-class-properties', {loose: true}]],
     },
   );
 };
